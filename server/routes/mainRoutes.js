5fef1d5780c08a0cb0a2fc3af2ce506576196321
Kevin Fechner <bkevinfechner1989@gmail.com>
2018-01-07 15:05:51 +0100

    add archiver for zipping bills

--- server/routes/mainRoutes.js
+++ server/routes/mainRoutes.js
@@ -49,6 +49,8 @@ router.post('/create-json', (req, res) => {
   fs.writeFile(`abrechnung-${billDate}.json`, jsonString, 'utf8');
   const rootDir = path.join(__dirname, '../public', '..', '..');
 
+  let allBills = Object.keys(dataDetails).length;
+  let currentBill = 0;
   for (let x in dataDetails) {
 
   // electron is 'stupid' and cannot handle POST REQuests
@@ -58,8 +60,6 @@ router.post('/create-json', (req, res) => {
     let registration = dataDetails[artist].artistDetails.registration;
     let billNumber = hf_PDF.calculateBillNumber(initBillNumber, registration, billDate);
     let artistID = dataDetails[artist].artistDetails.artistNummer;
-    // console.log(x);
-// https://nodejs.org/api/child_process.html
     setTimeout(function() {
       var cmd = `/usr/local/bin/node ${rootDir}/server/nightmare-pdf-test.js '${artist}' '${billDate}' '${artistID}' '${billNumber}'`;
       // var cmd = `/usr/local/bin/node ${rootDir}/server/playground/test.js`;
@@ -69,51 +69,15 @@ router.post('/create-json', (req, res) => {
           console.log(`exec error: ${err}`);
           return;
         }
+        currentBill += 1;
+        if (currentBill === allBills) {
+          hf_PDF.archivePDFS(billDate);
+        }
         console.log('stdout: ', stdout);
         console.log('stderr: ', stderr);
       });
      }, 3000);
-
-    // var cmd = `/usr/local/bin/node ${rootDir}/server/nightmare-pdf-test.js '${artist}' '${billDate}' '${artistID}' '${billNumber}'`;
-    // // var cmd = `/usr/local/bin/node ${rootDir}/server/playground/test.js`;
-    // // console.log("artist:", artist);
-    // exec(cmd, (err, stdout, stderr) => {
-    //   if (err) {
-    //     console.log(`exec error: ${err}`);
-    //     return;
-    //   }
-    //   console.log('stdout: ', stdout);
-    //   console.log('stderr: ', stderr);
-    // });
-
-
   }
-
-    // let cmdCall = new Promise((resolve, reject) => {
-    //   var cmd = `/usr/local/bin/node ${rootDir}/server/nightmare-pdf-test.js ${artist} ${billDate}`;
-    //   // var cmd = `/usr/local/bin/node ${rootDir}/server/playground/test.js`;
-    //   console.log("cmd:", cmd);
-    //   exec(cmd, (err, stdout, stderr) => {
-    //     if (err) {
-    //       console.log(`exec error: ${err}`);
-    //       return;
-    //     }
-    //     console.log('stdout: ', stdout);
-    //     console.log('stderr: ', stderr);
-    //     resolve('Success');
-    //
-    //   });
-    // });
-    // cmdCall.then((successMessage) => {
-    //   console.log(successMessage);
-    //   res.send('end of pdf_redirect');
-    //
-    // }).catch((err) => {
-    //   console.log("err: ", err);
-    // });
-
-
-  // res.redirect('/pdf-create');
 });
 
 router.get('/pdf-create', (req,res) => {
@@ -162,47 +126,7 @@ router.get('/pdf-create', (req,res) => {
 
 router.get('/test', (req, res) => {
 
-  const artist = "Der Kolibri";
-  const date = "2017-11";
-  console.log(`artist: ${artist}, date: ${date}`);
-  fs.readFile(`abrechnung-${date}.json`, (err, data) => {
-      if (err) {
-        throw err;
-      }
-      var parsedJSON = JSON.parse(data);
-      var billNumberDate = parsedJSON.abrechnungsBeginn;
-      var billingEnd = parsedJSON.abrechnungsEnde;
-      parsedJSON = parsedJSON.data[artist];
-      // console.log("new parsedJSON:", parsedJSON);
-      var firstName = parsedJSON.artistDetails.vorname;
-      var lastName = parsedJSON.artistDetails.nachname;
-      var brutto = parsedJSON.artistDetails.brutto == '1' ? true : false;
-
-
-      var from = moment(billNumberDate).format('DD.MM.YYYY');
-      var to = moment(billingEnd).format('DD.MM.YYYY');
-      var billNumberDateWithHiphens = moment(billNumberDate).format('YYYY-MM');
-      var billNumberDateWithoutHiphens = moment(billNumberDate).format('YYYYMM');
-      var initBillNumber = parsedJSON.artistDetails.init_bill_number;
-      var registration = parsedJSON.artistDetails.registration;
-      var billNumber = hf_PDF.calculateBillNumber(initBillNumber, registration, billNumberDate);
-      if (billNumber < initBillNumber) {
-        throw new Error('earlier than new Billing system');
-      }
-      res.render('pdf-create', {
-        title: 'PDF Create',
-        page: 'pdf-create',
-        data: parsedJSON,
-        billNumberDateWithoutHiphens: billNumberDateWithoutHiphens,
-        billNumber: billNumber,
-        brutto,
-        from: from,
-        to: to,
-        firstName,
-        lastName
-        // data: data
-      });
-    });
+  hf_PDF.archivePDFS('2017-11');
 });
 
 module.exports = router;
--- utils/create-pdf_helper_functions.js
+++ utils/create-pdf_helper_functions.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const fs = require('fs');
+
+const archiver = require('archiver');
 const moment = require('moment');
+
 
 exports.calculateBillNumber = function(initBillNumber, registration, billingMonth) {
   var billMonthDate = moment(billingMonth).format('YYYY-MM');
@@ -14,3 +18,27 @@ exports.calculateBillNumber = function(initBillNumber, registration, billingMont
 
   return billNumber;
 };
+
+exports.archivePDFS = function(date) {
+  var formattedDate = parseInt(moment(date).format('YYYYMM'));
+  const dateWithHiphens = moment(date).format('YYYY-MM');
+  var output = fs.createWriteStream(`./ABRECHNUNGEN_${dateWithHiphens}.zip`);
+  var archive = archiver('zip', {
+    gzip: true,
+    zlib: { level: 9}
+  });
+
+  archive.on('error', function(err) {
+    throw err;
+  });
+
+  archive.pipe(output);
+
+  fs.readdir('.', function(err, items) {
+    const billsToZip = items.filter(item => item.indexOf(formattedDate) > -1);
+    billsToZip.forEach((bill) => {
+      archive.file(bill, {name: bill});
+    })
+    archive.finalize();
+});
+}
